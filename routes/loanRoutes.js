1aa1d325a557915f8e8a6d1d34c45452d35d009d
diegohenao819 <diegohenao819@gmail.com>
2024-08-01 09:27:49 -0500

    chore: Update loanRoutes.js to add authentication middleware for getLoans route

--- routes/loanRoutes.js
+++ routes/loanRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
-const { getLoans, createLoan } = require('../controllers/loanController');
-
+const { getLoans, createLoan, deleteLoan } = require('../controllers/loanController');
+const authenticateToken = require('../middlewares/authMiddleware'); // Importa el middleware de autenticación
 const router = express.Router();
 
-router.get('/loans', getLoans);
-router.post('/loans', createLoan);
+// Obtener todos los préstamos
+router.get('/loans', authenticateToken, getLoans);
+
+// Crear un nuevo préstamo
+router.post('/loans', authenticateToken, createLoan);
+
+// Eliminar un préstamo
 router.delete('/loans', authenticateToken, deleteLoan);
 
 module.exports = router;
